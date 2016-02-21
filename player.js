317cf6acddaa73119cbb6c4f9202098e3ff88917
Nyctef <nyctef@nyctef.com>
2016-02-21 13:32:15 +0000

    implement really basic falling behaviour

--- player.js
+++ player.js
@@ -1,11 +1,25 @@
 var Player = function(game) {
   this.game = game
-  this.keyboard = new Keyboard(),
-  this.position = { x: 50, y: 50 }
+  this.keyboard = new Keyboard()
+  this.position = { x: 60, y: 50 }
   this.draw = function(screen) {
     screen.drawCircle(this.position.x, this.position.y, 1, 'green')
   }
   this.update = function() {
+    this.state()
+  }
+
+  this.falling = function() {
+    if (this.game.mapDataAt(this.position.x, this.position.y+1).isEdge ||
+        this.game.mapDataAt(this.position.x, this.position.y+2).isEdge) {
+      this.state = this.standing
+    }
+    else {
+      this.position.y += 2
+    }
+  }
+
+  this.standing = function() {
     var newX
     if (this.keyboard.isDown(this.keyboard.Keys.LEFT)) {
       newX = this.position.x - 2
@@ -21,7 +35,13 @@ var Player = function(game) {
         !this.game.mapDataAt(newX  , this.position.y).isEdge) {
       this.position.x = newX
     }
+    if (!this.game.mapDataAt(newX, this.position.y+1).isEdge) {
+      this.state = this.falling
+      return
+    }
   }
+
+  this.state = this.falling
 }
 
 // TODO: pull out control code and start working on climbing / jumping / falling behaviours

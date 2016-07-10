40a389dc6dfd87af45284cc3214712e84a38f5f6
Nyctef <nyctef@nyctef.com>
2016-07-11 00:33:59 +0100

    add second test for counterexample

--- test/foo-test.js
+++ test/foo-test.js
@@ -22,7 +22,19 @@ describe('VelocitySystem', () => {
 
       this.vs.set_move_plan(null, e)
 
+      // with a velocity of 1, we want to move 1 on 1/60 frames
       expect(e.move_plan.x).to.equal(1)
+    })
+    it('does not set the move plan in other frames', function() {
+
+      var e = new TestEntity()
+      e.velocity.x = 1
+      this.vs.start_frame()
+
+      this.vs.set_move_plan(null, e)
+
+      // with a velocity of 1, we only want to actually move every 60 frames
+      expect(e.move_plan.x).to.equal(0)
     })
   })
 })

4cb19bdb80036d1db8ffe6fd84212a3f285bbae0
Mark Jordan <mark.jordan@red-gate.com>
2016-07-15 17:09:51 +0100

    this code was moved into math.js

--- src/map.js
+++ src/map.js
@@ -4,11 +4,6 @@ function PixelData() {
   this.isEdge = false
 }
 
-function Point(x, y) {
-  this.x = x
-  this.y = y
-}
-
 var edge = new PixelData()
 edge.isEdge = true
 
@@ -70,38 +65,6 @@ function Map(initialImageData) {
     if (x<0 || x>=this._w ||
         y<0 || y>=this._h) { return edge }
     return this._mapData[(y*this._w) + x]
-  }
-
-  var noop = () => {}
-
-  this.castLine = function(predicate, x0, y0, x1, y1, doLog=noop) {
-    // based on drawLine/Bresenham's - iterate along the line and stop if we see an edge
-    var dx = Math.abs(x1-x0)
-    var dy = Math.abs(y1-y0)
-    var sx = (x0 < x1) ? 1 : -1
-    var sy = (y0 < y1) ? 1 : -1
-    var err = dx-dy
-
-    while(true) {
-      doLog(`castLine: checking predicate at ${x0},${y0}`)
-      if (predicate(x0, y0)) { 
-        doLog(`castLine: predicate returned true at ${x0},${y0}`)
-        return new Point(x0, y0)
-      }
-
-      if ((x0==x1) && (y0==y1)) {
-        doLog(`castLine: reached end of the line at ${x0},${y0}`)
-        return null
-      }
-      var e2 = 2*err
-      if (e2 >-dy){ err -= dy; x0  += sx }
-      if (e2 < dx){ err += dx; y0  += sy }
-    }
-  }
-
-  this.castLineToEdge = function(x0, y0, x1, y1) {
-    var isEdge = (x, y) => this.mapDataAt(x, y).isEdge
-    return this.castLine(isEdge, x0, y0, x1, y1)
   }
 
   this.explodeHole = function(x0, y0, radius) {

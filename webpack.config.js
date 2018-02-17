7b0be1c196d3f9efa3c6094d35bd4ff076615398
Nyctef <nyctef@nyctef.com>
2018-02-17 23:22:29 +0000

    fix things in webpack config that webpack complains about

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,23 @@
 module.exports = {
-    entry: "./src/game.js",
-    output: {
-        filename: "./dist/bundle.js",
-    },
+  entry: "./src/game.js",
+  output: {
+    filename: "./dist/bundle.js"
+  },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
-    },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
 
-    module: {
-        loaders: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
-        ],
-
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
-        ]
-    }
+  module: {
+    rules: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+    ]
+  }
 };
